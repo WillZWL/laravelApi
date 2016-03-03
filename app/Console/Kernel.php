29829ee94497615ad6516ab38ebcbfd8b2fef265
Handy Hon <handy.hon@eservicesgroup.com>
2016-03-03 19:57:48 +0800

    add table prefix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,6 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('order:retrieve')
-            ->everyFiveMinutes();
+            ->daily();
     }
 }
