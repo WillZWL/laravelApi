d82b1328d500d558a52dd13a0eba08fe410bac60
Handy Hon <handy.hon@eservicesgroup.com>
2016-03-05 14:08:25 +0800

    add acknowlege field to amazon_order table

---
+++ database/migrations/2016_03_04_101208_add_acknowledge_to_amazon_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAcknowledgeToAmazonOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('amazon_orders', function (Blueprint $table) {
+            $table->tinyInteger('acknowledge')->default(0)->after('latest_delivery_date')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('amazon_orders', function (Blueprint $table) {
+            //
+        });
+    }
+}
