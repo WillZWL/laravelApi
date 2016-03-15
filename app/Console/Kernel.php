0d212322c294ff28a696e77fa1bf39df7d7422a3
Handy Hon <handy.hon@eservicesgroup.com>
2016-03-15 17:58:37 +0800

    change cron job time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,9 +29,9 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')
         //          ->hourly();
         $schedule->command('order:retrieve')
-            ->dailyAt('01:02');
+            ->dailyAt('0:50');
 
         $schedule->command('order:transfer')
-            ->dailyAt('02:00');
+            ->dailyAt('01:00');
     }
 }
