f0f50023632464debf321b248d29bca81f51e8a4
Handy Hon <handy.hon@eservicesgroup.com>
2016-03-27 22:34:00 +0800

    finish add assembly product to order

---
+++ app/Models/ProductAssemblyMapping.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductAssemblyMapping extends Model
+{
+    public $connection = 'mysql_esg';
+
+    protected $table = 'product_assembly_mapping';
+
+    public $timestamps = false;
+
+    public $incrementing = false;
+
+    protected $guarded = ['create_at'];
+
+    public function product()
+    {
+        return $this->belongsTo('App\Models\Product', 'sku', 'sku');
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->where('product_assembly_mapping.status', '=', 1);
+    }
+}
