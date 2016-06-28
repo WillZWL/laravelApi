8e00c6554f0659b6e441a146b8b20b40e2e7c2b4
Handy Hon <handy.hon@eservicesgroup.com>
2016-06-28 10:11:52 +0800

    Pricing tool UI

--- resources/views/pricing/platform-pricing-info.blade.php
+++ resources/views/pricing/platform-pricing-info.blade.php
@@ -1,3 +1,16 @@
+<?php
+$link = [
+        'BCAMAZONGB' => 'https://www.amazon.co.uk/dp/',
+        'BCAMAZONUS' => 'https://www.amazon.com/dp/',
+        'BCAMAZONFR' => 'https://www.amazon.fr/dp/',
+        'BCAMAZONCA' => 'https://www.amazon.ca/dp/',
+        'BCAMAZONMX' => 'https://www.amazon.com.mx/dp/',
+        'BCAMAZONDE' => 'https://www.amazon.de/dp/',
+        'BCAMAZONES' => 'https://www.amazon.es/dp/',
+        'BCAMAZONIT' => 'https://www.amazon.it/dp/',
+        'BCAMAZONJP' => 'https://www.amazon.jp/dp/',
+];
+?>
 @foreach($data as $platform => $platformInfo)
     <div class="panel-heading" role="tab" id="head_{{ $platform }}">
         <h4 class="panel-title">
@@ -5,16 +18,37 @@
                aria-expanded="true" aria-controls="collapseTwo">
                 {{ $platform }}
             </a>
+            <a href="{{ $link[$platform].$platformInfo['asin'] }}" target="_blank">{{ $link[$platform].$platformInfo['asin'] }}</a>
         </h4>
     </div>
     <div id="{{ $platform }}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="head_{{ $platform }}"
          aria-expanded="true">
         <div class="panel-body">
             @if(count($platformInfo['deliveryOptions']) > 0)
-                <table class="table table-bordered table-condensed">
+                <table class="table table-bordered table-condensed" style="table-layout: fixed">
+                    <colgroup>
+                        <col width="7%">
+                        <col width="8%">
+                        <col width="4%">
+                        <col width="4%">
+                        <col width="4%">
+                        <col width="5%">
+                        <col width="5%">
+                        <col width="5%">
+                        <col width="4%">
+                        <col width="3%">
+                        <col width="5%">
+                        <col width="5%">
+                        <col width="5%">
+                        <col width="5%">
+                        <col width="5%">
+                        <col width="5%">
+                        <col width="5%">
+                        <col width="8%">
+                        <col width="5%">
+                    </colgroup>
                     <tbody>
                     <tr class="info">
-                        <th>Type</th>
                         <th>Delivery Type</th>
                         <th>Price</th>
                         <th>Decl.</th>
@@ -37,7 +71,6 @@
                     </tr>
                     @foreach($platformInfo['deliveryOptions'] as $deliveryType => $item)
                         <tr>
-                            <td>Cost</td>
                             <td>
                                 <div class="radio">
                                     <label>
@@ -46,7 +79,7 @@
                                 </div>
                             </td>
                             <td>
-                                <input name="price" style="width: 70px" value="{{ $item['price'] }}" data-marketplace-sku="{{ $item['marketplaceSku'] }}" data-selling-platform="{{ $platform }}">
+                                <input name="price" style="width: 100%" value="{{ $item['price'] }}" data-marketplace-sku="{{ $item['marketplaceSku'] }}" data-selling-platform="{{ $platform }}">
                             </td>
                             <td>{{ $item['declaredValue'] }}</td>
                             <td>{{ $item['tax'] }}</td>
@@ -68,37 +101,41 @@
                         </tr>
                     @endforeach
                     <tr>
-                        <td>Listing Status</td>
-                        <td>
+                        <td colspan="2">Listing Status</td>
+                        <td colspan="7">
                             <select name="listingStatus" required="required">
                                 <option value="">-- Select --</option>
                                 <option value="Y" {{ ($platformInfo['listingStatus'] == 'Y') ? 'selected' : '' }}>Listed</option>
                                 <option value="N" {{ ($platformInfo['listingStatus'] == 'N') ? 'selected' : '' }}>Not listed</option>
                             </select>
                         </td>
-                        <td>Inventory</td>
-                        <td>
+                        <td colspan="3">Inventory</td>
+                        <td colspan="7">
                             <input type="text" name="inventory" id="inputInventory" value="{{ $platformInfo['inventory'] }}" required="required">
                         </td>
-                        <td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
                             <a href="/images/amazon_latency.jpg" target="_blank">
                                 Latency
                             </a>
                         </td>
-                        <td>
+                        <td colspan="7">
                             <input type="text" name="latency" id="inputLatency" value="{{ $platformInfo['fulfillmentLatency'] }}" required="required">
                         </td>
 
-                        <td>
+                        <td colspan="3">
                             Amazon Brand
                         </td>
-                        <td>
+                        <td colspan="7">
                             <input type="text" name="platformBrand" id="inputPlatformBrand" value="{{ $platformInfo['platformBrand'] }}">
                         </td>
-                        <td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
                             Condition
                         </td>
-                        <td>
+                        <td colspan="7">
                             <select name="condition" id="condition">
                                 <option value="New" {{ ($platformInfo['condition'] == 'New') ? 'selected' : '' }}>New</option>
                                 <option value="UsedLikeNew" {{ ($platformInfo['condition'] == 'UsedLikeNew') ? 'selected' : '' }}>UsedLikeNew</option>
@@ -113,17 +150,15 @@
                                 <option value="Club" {{ ($platformInfo['condition'] == 'Club') ? 'selected' : '' }}>Club</option>
                             </select>
                         </td>
-                    </tr>
-                    <tr>
-                        <td>
+                        <td colspan="3">
                             Condition Note
                         </td>
-                        <td colspan="19">
+                        <td colspan="7">
                             <input type="text" name="conditionNote" id="conditionNote" value="{{ $platformInfo['conditionNote'] }}">
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="20" align="center">
+                        <td colspan="19" align="center">
                             <button type="button" data-platform="{{ $platform }}"
                                     class="btn btn-danger save_price_info"> Save
                             </button>
