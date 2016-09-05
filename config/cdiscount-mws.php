d02c8b5a8914ba6305947803ae4250939323391f
gao jinbin <gao1963bin@gmail.com>
2016-09-05 19:50:52 +0800

    update

---
+++ config/cdiscount-mws.php
@@ -0,0 +1,17 @@
+<?php
+
+// For Cdiscount Config
+return [
+    'store' => [
+        '' => [
+            'name' => 'Brand Connect Cdiscount',
+            'userId' => '',
+            'password' => '',
+            'currency'=>'CZK',
+        ], 
+    ],
+    
+    // Default service URL
+    'SERVICE_URL' => '',
+    'muteLog' => true
+];
