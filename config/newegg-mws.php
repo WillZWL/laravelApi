c84d467d5e4312226a0b32b70066de6e0124495e
ping <ping@eservicesgroup.com>
2016-09-05 17:53:29 +0800

    no message

---
+++ config/newegg-mws.php
@@ -0,0 +1,21 @@
+<?php
+
+// For Newegg Config
+return [
+    'store' => [
+        'BCNEWEGGUS' => [
+            'name' => 'Brand Connect Newegg',
+            'userId' => 'portal.sandbox.41@gmail.com',
+            'password' => '8YbV6gDu',
+            'currency'=>'USD',
+            'sellerId' => "ABVF",
+            'neweggServiceUrl' => "https://api.newegg.com/marketplace/",
+            'apiKey' => "bc3e1dfb4b535de8761427c771ddd37c",
+            'secretKey' => "cc4743c4-be1e-466b-9b49-d346524df6fe"
+        ]
+    ],
+
+    // Default service URL
+    'NEWEGG_SERVICE_URL' => 'https://api.newegg.com/marketplace/',
+    'muteLog' => true
+];
