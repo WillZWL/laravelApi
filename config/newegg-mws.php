ba2cf6ff360342ec55cf2ea2f248b334483c2f01
ping <ping@eservicesgroup.com>
2016-09-05 16:29:24 +0800

    SBF#10337 - Newegg login

--- config/newegg-mws.php
+++ config/newegg-mws.php
@@ -5,13 +5,17 @@ return [
     'store' => [
         'BCNEWEGGUS' => [
             'name' => 'Brand Connect NewEgg',
-            'userId' => 'newegg@brandsconnect.net',
+            'userId' => 'portalsandbox06+A2A6@gmail.com',
             'password' => 'aCC21salES5k',
             'currency'=>'USD',
+            'sellerId' => "A2A6",
+            'neweggServiceUrl' => "https://api.newegg.com/marketplace/contentmgmt/",
+            'apiKey' => "7172b5061a6e3085e01e665681b1fba2",
+            'secretKey' => "88d82cb8-3c7e-49b4-b921-abf1d1f9f9a8"
         ]
     ],
 
     // Default service URL
-    'SERVICE_URL' => '',
+    'NEWEGG_SERVICE_URL' => 'https://api.newegg.com/marketplace/contentmgmt/',
     'muteLog' => true
 ];
