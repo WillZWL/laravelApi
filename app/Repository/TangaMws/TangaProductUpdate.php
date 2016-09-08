8d1eafd5d1b70e8748d55ec2acc9ee82830c3557
brave <brave.liu@eservicesgroup.com>
2016-09-08 16:59:01 +0800

    tanga list feed

---
+++ app/Repository/TangaMws/TangaProductUpdate.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Repository\TangaMws;
+
+class TangaProductUpdate extends TangaOrderCore
+{
+    private $vendorSkuCode;
+    private $inStock;
+
+    public function __construct($store)
+    {
+        parent::__construct($store);
+        $this->settangaPath();
+    }
+
+    public function updateInventoryToTanga()
+    {
+        return $this->query($this->getRequestParams(), 'POST');
+    }
+
+    protected function getRequestParams()
+    {
+        if ($this->getVendorSkuCode()) {
+            $requestParams['vendor_sku_code'] = $this->getVendorSkuCode();
+        }
+
+        if ($this->getInStock() >= 0) {
+            $requestParams['in_stock'] = $this->getInStock();
+        }
+
+        return $requestParams;
+    }
+
+    public function setTangaPath()
+    {
+        $this->tangaPath = 'api/v1/drop_shippers/'. $this->vendorAppId .'/inventory';
+    }
+
+
+    public function getVendorSkuCode()
+    {
+        return $this->vendorSkuCode;
+    }
+
+    public function setVendorSkuCode($vendorSkuCode)
+    {
+        $this->vendorSkuCode = $vendorSkuCode;
+    }
+
+    public function getInStock()
+    {
+        return $this->inStock;
+    }
+
+    public function setInStock($inStock)
+    {
+        $this->inStock = $inStock;
+    }
+}
