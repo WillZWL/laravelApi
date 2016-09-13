1d054267d41ab9acda6f801778ee2b3a67f3a1e2
Handy Hon <handy.hon@eservicesgroup.com>
2016-09-13 20:45:48 +0800

    Update pricing overview

---
+++ app/Search/Filters/Inventory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Search\Filters;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class Inventory implements Filter
+{
+    /**
+     * Apply a given search value to the builder instance.
+     *
+     * @param Builder $builder
+     * @param mixed $value
+     * @return Builder $builder
+     */
+    public static function apply(Builder $builder, $value)
+    {
+        return $builder->whereHas('inventory', function ($q) use ($value) {
+            return $q->where('inventory.inventory', '<', $value);
+        });
+    }
+}
