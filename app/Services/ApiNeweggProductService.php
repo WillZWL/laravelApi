cb9090da52a3358d78ae7ad23fbe1eb41703b9e1
WILLY\willy <willy.dharman@eservicesgroup.com>
2016-10-07 14:28:48 +0800

    Newegg_inventory submit

--- app/Services/ApiNeweggProductService.php
+++ app/Services/ApiNeweggProductService.php
@@ -26,7 +26,7 @@ class ApiNeweggProductService extends ApiBaseService implements ApiPlatformProdu
     public function submitProductPriceAndInventory($storeName)
     {
         $this->submitProductPrice($storeName);
-        //$this->submitProductInventory($storeName);
+        $this->submitProductInventory($storeName);
     }
 
     public function submitProductPrice($storeName)
@@ -101,49 +101,106 @@ class ApiNeweggProductService extends ApiBaseService implements ApiPlatformProdu
 
     public function submitProductInventory($storeName)
     {
+        //For debugging purpose, uncomment the following line to get the list of items to update
+        //dd(print_r($this->getProductInventory($storeName)));
+
         $processStatusProduct = MarketplaceSkuMapping::ProcessStatusProduct($storeName,self::PENDING_INVENTORY);
         if(!$processStatusProduct->isEmpty())
         {
             $failed_sku_list = array();
+            $failed_sku_name = array();
             $successful_sku_list = array();
-
+            $counterSku = 0;
             foreach ($processStatusProduct as $index => $pendingSku) 
             {
+                //Set the sku counter to ensure the api calling doesn't reach the limit of 300 per min
+                $counterSku++;
+                if ($counterSku > 300) //once limit, sleep for 1 min before continue to the next 300 skus
+                {
+                    sleep(60);
+                    $counterSku = 0;
+                }
+
+                //First get the warehouse availability for each item
                 $requestXml = array();
-                $requestXml[] = '<ItemInventoryInfo>';
+                $requestXml[] = '<ContentQueryCriteria>';
                 $requestXml[] =     '<Type>1</Type>';
                 $requestXml[] =     '<Value>'.$pendingSku->marketplace_sku.'</Value>';
-                $requestXml[] =     '<InventoryList>';
-                $requestXml[] =         '<Inventory>';
-                $requestXml[] =             '<WarehouseLocation>'.$pendingSku->id_3_digit.'</WarehouseLocation>';
-                $requestXml[] =             '<AvailableQuantity>'.$pendingSku->inventory.'</AvailableQuantity>';
-                $requestXml[] =         '</Inventory>';
-                $requestXml[] =     '</InventoryList>';
-                $requestXml[] = '</ItemInventoryInfo>';
+                $requestXml[] = '</ContentQueryCriteria>';
                 $requestXml = implode("\n", $requestXml);
                 $requestParams = $this->getRequestParams();
                 $this->neweggCore = new NeweggCore($storeName);
-                $result = $this->neweggCore->query("contentmgmt/item/international/inventory", $this->getResourceMethod(), $requestParams, $requestXml);
-                if($result)
-                {
-                    if($result["error"])
-                    {
-                        $error_string = $result["error"][2];
-                        $error_split = explode("\"",$error_string);
+                $result = $this->neweggCore->query("contentmgmt/item/international/inventory", "PUT", $requestParams, $requestXml);
+                unset($requestXml); //clear the requestXml before start the next api call
 
-                        $failed_sku_list[$pendingSku->marketplace_sku][$pendingSku->id_3_digit] = $error_split[7];
-                        $failed_sku_name[$pendingSku->marketplace_sku][$pendingSku->id_3_digit] = $pendingSku->name; // SBF#10337 add product name                        
+                //Finally, update the inventory of the current item
+                //But before we proceed, check if the current item retrieved have error or not, proceed only if current item got no error
+                if ($result["error"])
+                {
+                    $error_string = $result["error"][2];
+                    $error_split = explode("\"",$error_string);
+
+                    $failed_sku_list[$pendingSku->marketplace_sku] = $error_split[7];
+                    $failed_sku_name[$pendingSku->marketplace_sku] = $pendingSku->name; // SBF#10337 add product name                        
+                }
+
+                else
+                {
+                    $requestXml = array();
+                    $requestXml[] = '<ItemInventoryInfo>';
+                    $requestXml[] =     '<Type>1</Type>';
+                    $requestXml[] =     '<Value>'.$pendingSku->marketplace_sku.'</Value>';
+                    $requestXml[] =     '<InventoryList>';
+
+                    //Updating each item inventory in all available warehouses                
+                    foreach ($result["data"]["InventoryAllocation"] as $key => $inventoryDetails) 
+                    {
+                        $requestXml[] =         '<Inventory>';
+
+                        //For debugging purpose, uncomment the if else below will forcing one sku to use wrong warehouse (18593-AA-NA)
+                        //if ($pendingSku->marketplace_sku == '18593-AA-BK')
+                        //   $requestXml[] =             '<WarehouseLocation>HKG</WarehouseLocation>';
+                        //else
+                            $requestXml[] =             '<WarehouseLocation>'.$inventoryDetails["WarehouseLocation"].'</WarehouseLocation>';
+
+                        $requestXml[] =             '<AvailableQuantity>'.$pendingSku->inventory.'</AvailableQuantity>';
+                        $requestXml[] =         '</Inventory>';
+                    }
+
+                    $requestXml[] =     '</InventoryList>';
+                    $requestXml[] = '</ItemInventoryInfo>';
+                    $requestXml = implode("\n", $requestXml);
+                    $requestParams = $this->getRequestParams();
+                    $this->neweggCore = new NeweggCore($storeName);
+                    $result = $this->neweggCore->query("contentmgmt/item/international/inventory", $this->getResourceMethod(), $requestParams, $requestXml);
+                    
+                    if($result)
+                    {
+                        if($result["error"])
+                        {
+                            $error_string = $result["error"][2];
+                            $error_split = explode("\"",$error_string);
+
+                            $failed_sku_list[$pendingSku->marketplace_sku] = $error_split[7];
+                            $failed_sku_name[$pendingSku->marketplace_sku] = $pendingSku->name; // SBF#10337 add product name                        
+                        }
+                        else
+                        {
+                            $this->updatePendingProductProcessStatusBySku($pendingSku,self::PENDING_INVENTORY);
+
+                            //Set up successful update message per warehouse 
+                            foreach ($result["data"]["InventoryList"] as $key => $newInventoryDetails) 
+                            {  
+                                $successful_sku_list[$pendingSku->marketplace_sku][$newInventoryDetails["WarehouseLocation"]]["ProductName"] = "[".$counterSku."] ".$pendingSku->name; // SBF#10337 add product name
+                                $successful_sku_list[$pendingSku->marketplace_sku][$newInventoryDetails["WarehouseLocation"]]["UpdatedInventory"] = $newInventoryDetails["AvailableQuantity"];
+                            }
+                        }
                     }
                     else
                     {
-                        $this->updatePendingProductProcessStatusBySku($pendingSku,self::PENDING_INVENTORY);
-                        $successful_sku_list[$pendingSku->marketplace_sku][$pendingSku->id_3_digit] = $pendingSku->inventory;
+                        $failed_sku_list[$pendingSku->marketplace_sku] = "Failed to update this sku inventory";
+                        $failed_sku_name[$pendingSku->marketplace_sku] = $pendingSku->name;
                     }
-                }
-                else
-                {
-                    $failed_sku_list[$pendingSku->marketplace_sku][$pendingSku->id_3_digit] = "Failed to update this sku inventory";
-                    $failed_sku_name[$pendingSku->marketplace_sku][$pendingSku->id_3_digit] = $pendingSku->name;
                 }
             }
 
@@ -151,15 +208,11 @@ class ApiNeweggProductService extends ApiBaseService implements ApiPlatformProdu
             {
                 $subject = "[NEWEGG] Inventory update failed!";
                 $message = "The following sku has error in updating their inventory to newegg marketplace. \r\n\r\n";
-                foreach ($failed_sku_list as $key => $failed_sku) 
+                foreach ($failed_sku_list as $key => $failed_sku_message) 
                 {
-                    foreach ($failed_sku as $warehouse_code => $error_message) 
-                    {
-                        $message.="Marketplace SKU: ".$key."\r\n";
-                        $message.="Product Name: ".$failed_sku_name[$key][$warehouse_code]."\r\n";
-                        $message.="Warehouse Location: ".$warehouse_code."\r\n";
-                        $message.="Message: ". $error_message."\r\n\r\n\n";    
-                    }
+                    $message.="Marketplace SKU: ".$key."\r\n";
+                    $message.="Product Name: ".$failed_sku_name[$key]."\r\n";
+                    $message.="Message: ". $failed_sku_message."\r\n\r\n\n";    
                 }
                 $message .= "\r\nThanks\r\n";
                 $this->sendInventoryAlertMailMessage($subject, $message, 0);
@@ -168,14 +221,15 @@ class ApiNeweggProductService extends ApiBaseService implements ApiPlatformProdu
             if ($successful_sku_list)
             {
                 $subject = "[NEWEGG] Inventory update success!";
-                $message = "The following sku have succeeded in updating their inventory to newegg marketplace. \r\n\r\n";
+                $message = "The following sku have succeeded in updating their inventory to Newegg marketplace. \r\n\r\n";
                 foreach ($successful_sku_list as $key => $successful_sku) 
                 {
-                    foreach ($successful_sku as $warehouse_code => $quantity) 
+                    foreach ($successful_sku as $warehouse_code => $successfulDetails) 
                     {
                         $message.="Marketplace SKU: ".$key."\r\n";
-                        $message.="Updated inventory: ".$quantity."\r\n";
-                        $message.="Warehouse Location: ".$warehouse_code."\r\n\n";                    
+                        $message.="Product Name: ".$successfulDetails["ProductName"]."\r\n";                        
+                        $message.="Warehouse Location: ".$warehouse_code."\r\n";
+                        $message.="Updated inventory: ".$successfulDetails["UpdatedInventory"]."\r\n\n";
                     }
                 }
                 $message .= "\r\nThanks\r\n";                
@@ -184,6 +238,29 @@ class ApiNeweggProductService extends ApiBaseService implements ApiPlatformProdu
             return true;
         }
         return false;
+    }
+
+    public function getProductInventory($storeName)
+    {
+        $processStatusProduct = MarketplaceSkuMapping::ProcessStatusProduct($storeName,self::PENDING_INVENTORY);
+        if(!$processStatusProduct->isEmpty())
+        {
+            foreach ($processStatusProduct as $index => $pendingSku) 
+            {
+                $requestXml = array();
+                $requestXml[] = '<ContentQueryCriteria>';
+                $requestXml[] =     '<Type>1</Type>';
+                $requestXml[] =     '<Value>'.$pendingSku->marketplace_sku.'</Value>';
+                $requestXml[] = '</ContentQueryCriteria>';
+                $requestXml = implode("\n", $requestXml);
+                $requestParams = $this->getRequestParams();
+                $this->neweggCore = new NeweggCore($storeName);
+                $result = $this->neweggCore->query("contentmgmt/item/international/inventory", "PUT", $requestParams, $requestXml);
+                $productInventoryList[] = $result;
+            }
+
+            return $productInventoryList;
+        }
     }
 
     public function submitProductCreate($storeName)
