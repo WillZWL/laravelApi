0ed3b656dda3bba4654c33ebf7945a9387aacb1f
Will <will@WilldeMacBook-Air.local>
2016-10-13 16:42:56 +0800

    update example file

--- app/Http/Controllers/Api/MattelSkuMappingController.php
+++ app/Http/Controllers/Api/MattelSkuMappingController.php
@@ -31,20 +31,22 @@ class MattelSkuMappingController extends Controller
 
     public function upload(Requests\MattelSkuMappingUploadRequest $mattelSkuMappingUploadRequest)
     {
-        $upload_dir = storage_path() .'/mattel-sku-mapping-upload/';
-        $options['upload_dir'] = $this->mkUploadDir($upload_dir);
-        $options['print_response'] = false;
-        try {
-            $upload_response = new JqueryFileUploadHandler($options);
-        } catch (\Exception $e) {
-            mail('will.zhang@eservicesgroup.com', 'Mattel SKU Mapping Upload Failed - Exception', $e->getMessage()."\r\n File: ".$e->getFile()."\r\n Line: ".$e->getLine());
+        if ($mattelSkuMappingUploadRequest->isMethod('post')) {
+            $upload_dir = storage_path() .'/mattel-sku-mapping-upload/';
+            $options['upload_dir'] = $this->mkUploadDir($upload_dir);
+            $options['print_response'] = false;
+            try {
+                $upload_response = new JqueryFileUploadHandler($options);
+                $response = $upload_response->response;
+            } catch (\Exception $e) {
+                mail('will.zhang@eservicesgroup.com', 'Mattel SKU Mapping Upload Failed - Exception', $e->getMessage()."\r\n File: ".$e->getFile()."\r\n Line: ".$e->getLine());
+            }
+            if ($response) {
+                $upload_file_path = $options['upload_dir'].$response['files'][0]->name;
+                $this->mappingService->handleUploadFile($upload_file_path);
+                return response()->json($response);
+            }
         }
-        $response = $upload_response->response;
-        if ($response) {
-            $upload_file_path = $options['upload_dir'].$response['files'][0]->name;
-            $this->mappingService->handleUploadFile($upload_file_path);
-        }
-        return response()->json($response);
     }
 
     public function mkUploadDir($dir)
