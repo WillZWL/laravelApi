02428190d5c262b7d6da63d98220c851549b60d6
brave <brave.liu@eservicesgroup.com>
2016-11-15 16:11:16 +0800

    FnacPT

--- config/fnac-mws.php
+++ config/fnac-mws.php
@@ -6,12 +6,19 @@ return [
         'BCFNACES' => [
             'name' => 'Brand Connect FNAC',
             'userId' => 'fnaces@brandsconnect.net',
-            'password' => '',
             'partnerId' => '8CB86115-9B7C-BBE0-3B2D-4B84934D4F5A',
             'shopId' => '331633EE-B9AE-ABDA-EE8E-C599C5DEA0E0',
             'key' => '82AB2BBF-A6C7-DEC6-9E17-D0FCA1F73C99',
             'currency'=>'EUR',
-        ]
+        ],
+        'BCFNACPT' => [
+            'name' => 'Brand Connect FNAC',
+            'userId' => 'fnacpt@brandsconnect.net',
+            'partnerId' => '8CB86115-9B7C-BBE0-3B2D-4B84934D4F5A',
+            'shopId' => '6350BF88-3192-16D9-8C2F-6EA2C5E29B9C',
+            'key' => '82AB2BBF-A6C7-DEC6-9E17-D0FCA1F73C99',
+            'currency'=>'EUR',
+        ],
     ],
 
     // Default service URL
