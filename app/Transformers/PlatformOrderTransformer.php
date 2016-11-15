4cc68979931acc470003086b8342dad6bd20a138
Will Zhang <will.zhang@eservicesgroup.com>
2016-11-15 15:30:52 +0800

    sbf#10631  DC Assembly SKU Mapping to Order Fulfillment

--- app/Transformers/PlatformOrderTransformer.php
+++ app/Transformers/PlatformOrderTransformer.php
@@ -3,27 +3,54 @@
 namespace App\Transformers;
 
 use App\Models\PlatformMarketOrder;
+use App\Models\ProductAssemblyMapping;
+use App\Models\PlatformMarketInventory;
 use League\Fractal\TransformerAbstract;
 
 class PlatformOrderTransformer extends TransformerAbstract
 {
     public function transform(PlatformMarketOrder $order)
     {
+        //get ordet item ['sku', 'qty', 'inventory']
+        $merchant = 'MATTEL';
         $items = [];
         foreach ($order->platformMarketOrderItem as $orderItem) {
-            $item['sku'] = $orderItem->seller_sku;
-            $item['qty'] = $orderItem->quantity_ordered;
-            $inventory = $orderItem->platformMarketInventory($order->store_id)->first();
-            $item['inventory'] = 0;
-            if ($inventory) {
-                $item['inventory'] = $inventory->inventory;
+            $esgSku = $orderItem->marketplaceSkuMapping()->first()->sku;
+            $marketplaceId = $orderItem->marketplaceSkuMapping()->first()->marketplace_id;
+
+            $assemblyMappings = ProductAssemblyMapping::active()->where('main_sku', $esgSku)
+                ->where('status', '=', '1')
+                ->get();
+
+            if (!$assemblyMappings->isEmpty()) {
+                //replace Assembly Sku
+                foreach ($assemblyMappings as $assemblySku) {
+                    $item['sku'] = $assemblySku->merchantProductMapping($merchant)->first()->merchant_sku;
+                    $item['qty'] = $orderItem->quantity_ordered * $assemblySku->replace_qty;
+
+                    $inventory = PlatformMarketInventory::where('mattel_sku', $item['sku'])->where('store_id', $order->store_id)->first();
+                    $item['inventory'] = 0;
+                    if ($inventory) {
+                        $item['inventory'] = $inventory->inventory;
+                    }
+                    $items[] = $item;
+                }
+            } else {
+                $item['sku'] = $orderItem->seller_sku;
+                $item['qty'] = $orderItem->quantity_ordered;
+                $inventory = $orderItem->platformMarketInventory($order->store_id)->first();
+                $item['inventory'] = 0;
+                if ($inventory) {
+                    $item['inventory'] = $inventory->inventory;
+                }
+                $items[] = $item;
             }
-            $items[] = $item;
         };
+
         return [
             'id' => $order->id,
             'biz_type' => $order->biz_type,
-            'merchant' => 'MATTEL',
+            'merchant' => $merchant,
             'platform_order_id' => $order->platform_order_no,
             'order_create_date' => $order->purchase_date,
             'payment_method' => $order->payment_method,
