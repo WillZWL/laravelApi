06a1c9e4ec3aec6278c398a5a5264e3761f9bc00
Handy Hon <handy.hon@eservicesgroup.com>
2016-11-15 15:13:41 +0800

    Rename Model

--- app/Models/AmazonProductSizeTier.php
+++ app/Models/AmazonProductSizeTier.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class amazon_product_size_tier extends Model
+class AmazonProductSizeTier extends Model
 {
     public function marketplaceSkuMapping()
     {
