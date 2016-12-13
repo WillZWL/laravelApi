b3ef4a7c1878c19b9a054dcece69a1d3be103250
chenqi <1005021821@qq.com>
2016-12-13 16:21:39 +0800

    code optimization

--- app/Http/Controllers/Api/PriceUploadController.php
+++ app/Http/Controllers/Api/PriceUploadController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
-use Dingo\Api\Routing\Helpers;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Websafe\Blueimp\JqueryFileUploadHandler;
@@ -11,36 +10,25 @@ use App\Services\PriceUploadService;
 
 class PriceUploadController extends Controller
 {
-    use Helpers;
-
-    public function __construct()
-    {
-
-    }
-
-    public function index()
-    {
-
-    }
-
-    public function upload(Requests\ProductUploadRequest $updateProductRequest)
+    public function upload()
     {
         $upload_dir = storage_path() .'/bulk-price-upload/';
         $options['upload_dir'] = $this->mkUploadDir($upload_dir);
         $options['print_response'] = false;
         try {
             $upload_response = new JqueryFileUploadHandler($options);
+
+            $response = $upload_response->response;
+
+            if ($response) {
+                $upload_file_path = $options['upload_dir'].$response['files'][0]->name;
+                $priceUploadService = New PriceUploadService();
+                $priceUploadService->handleUploadFile($upload_file_path);
+            }
+            return response()->json($response);
         } catch (\Exception $e) {
             mail('milo.chen@eservicesgroup.com', 'Price Upload Failed - Exception', $e->getMessage()."\r\n File: ".$e->getFile()."\r\n Line: ".$e->getLine());
         }
-        $response = $upload_response->response;
-
-        if ($response) {
-            $upload_file_path = $options['upload_dir'].$response['files'][0]->name;
-            $priceUploadService = New PriceUploadService();
-            $priceUploadService->handleUploadFile($upload_file_path);
-        }
-        return response()->json($response);
     }
 
     public function mkUploadDir($dir)
@@ -55,16 +43,5 @@ class PriceUploadController extends Controller
     {
         $file = \Storage::disk('priceUpload')->get($filename);
         return response($file, 200)->header('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    }
-
-    public function testExcelToDatabase()
-    {
-
-        $file = storage_path() .'/bulk-product-upload/2016/09/13/UploadExample.csv';
-
-        $productService = New ProductService();
-
-        $productService->handleUploadFile($file);
-
     }
 }
