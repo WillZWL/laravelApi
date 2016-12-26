a3f15c1e43fad25826d7dfe69dc3deb472d18b47
Handy Hon <handy.hon@eservicesgroup.com>
2016-12-26 12:29:56 +0800

    1st leg freight cost

--- app/Console/Commands/OrderTransfer.php
+++ app/Console/Commands/OrderTransfer.php
@@ -6,6 +6,7 @@ use App\Http\Requests\ProfitEstimateRequest;
 use App\Models\AcceleratorShipping;
 use App\Models\AmazonOrder;
 use App\Models\Client;
+use App\Models\Country;
 use App\Models\CountryState;
 use App\Models\CourierCost;
 use App\Models\CourierInfo;
@@ -132,7 +133,7 @@ class OrderTransfer extends Command
                 $alertEmail = 'handy.hon@eservicesgroup.com';
         }
 
-        //$alertEmail = 'handy.hon@eservicesgroup.com';
+//        $alertEmail = 'handy.hon@eservicesgroup.com';
 
         // check marketplace sku mapping
         $marketplaceSkuList = $order->amazonOrderItem->pluck('seller_sku')->toArray();
@@ -290,7 +291,9 @@ class OrderTransfer extends Command
             }
 
             // must after get correct delivery type id.
-            $spIncoterm = SpIncoterm::wherePlatformId($so->platform_id)->whereDeliveryTypeId($so->delivery_type_id)->first();
+            $spIncoterm = SpIncoterm::wherePlatformId($so->platform_id)
+                ->whereDeliveryTypeId($so->delivery_type_id)
+                ->first();
             if ($spIncoterm) {
                 $so->incoterm = $spIncoterm->incoterm;
             }
@@ -356,7 +359,8 @@ class OrderTransfer extends Command
         $client = $this->createOrUpdateClient($order);
         $newOrder->so_no = $this->generateSoNumber();
         $newOrder->platform_order_id = $order->amazon_order_id;
-        $newOrder->platform_id = 'AC-BCAZ-GROUPUS'; // it should depends on group order or split order. temporary set it this.
+        // it should depends on group order or split order. temporary set it this.
+        $newOrder->platform_id = 'AC-BCAZ-GROUPUS';
         $newOrder->txn_id = $order->amazon_order_id;
         $newOrder->client_id = $client->id;
         $newOrder->biz_type = 'AMAZON';
@@ -379,7 +383,6 @@ class OrderTransfer extends Command
             // set order import date as FBA dispatch date. Fiona required.
             $newOrder->dispatch_date = Carbon::now();
         } else {
-            // assume 'STD' first to fit database rule, need to decide it at createSplitOrder() and createGroupOrder() later.
             $newOrder->delivery_type_id = 'STD';
         }
         $newOrder->delivery_name = $order->amazonShippingAddress->name;
@@ -391,7 +394,10 @@ class OrderTransfer extends Command
         $newOrder->delivery_postcode = $order->amazonShippingAddress->postal_code;
         $newOrder->delivery_city = $order->amazonShippingAddress->city;
         $newOrder->delivery_country_id = $order->amazonShippingAddress->country_code;
-        $newOrder->delivery_state = CountryState::getStateId($order->amazonShippingAddress->country_code, $order->amazonShippingAddress->state_or_region);
+        $newOrder->delivery_state = CountryState::getStateId(
+            $order->amazonShippingAddress->country_code,
+            $order->amazonShippingAddress->state_or_region
+        );
         // if fulfillment by amazon, marked as shipped (6), if fulfillment by ESG, marked as 3, no need credit check.
         $newOrder->status = ($order->fulfillment_channel === 'AFN') ? '6' : '3';
         $newOrder->order_create_date = $order->purchase_date;
@@ -535,6 +541,7 @@ class OrderTransfer extends Command
     {
         $merchantId = $order->sellingPlatform->merchant_id;
         $deliveryCountry = $order->delivery_country_id;
+        $deliveryStateId = $order->delivery_state;
         $deliveryType = $order->delivery_type_id;
 
         $quotationTypes = DeliveryTypeMapping::where('delivery_type', $deliveryType)
@@ -579,9 +586,24 @@ class OrderTransfer extends Command
         } else {
             $shippingWeight = max($order->weight, $order->vol_weight);
         }
+
+        // for FBA order, freight cost is 1st leg cost, from HK to FBA warehouse.
+        if ($courier->type === 'FBA') {
+            $deliveryCountry = substr($order->platform_id, -2);
+            $deliveryState = Country::find($deliveryCountry)
+                ->countryState()
+                ->where('is_default_state', '=', 1)
+                ->first();
+            if (empty($deliveryState)) {
+                $deliveryStateId = '';
+            } else {
+                $deliveryStateId = $deliveryState->state_id;
+            }
+        }
+
         $weightId = WeightCourier::where('weight', '>=', $shippingWeight)->first()->id;
-        $courierCost = $courier->courierCost()->where('dest_country_id', $order->delivery_country_id)
-            ->where('dest_state_id', $order->delivery_state)
+        $courierCost = $courier->courierCost()->where('dest_country_id', $deliveryCountry)
+            ->where('dest_state_id', $deliveryStateId)
             ->where('weight_id', $weightId)
             ->orderBy('dest_state_id', 'DESC')
             ->first();
@@ -616,7 +638,12 @@ class OrderTransfer extends Command
         }
 
         if ($splitOrders[0]->courierInfo) {
-            $courierCost = $this->getCourierCost($order->delivery_country_id, $order->delivery_state, $weightId, $splitOrders[0]->courierInfo->courier_id);
+            $courierCost = $this->getCourierCost(
+                $order->delivery_country_id,
+                $order->delivery_state,
+                $weightId,
+                $splitOrders[0]->courierInfo->courier_id
+            );
 
             if ($courierCost) {
                 $currencyRate = ExchangeRate::getRate('HKD', $order->currency_id);
