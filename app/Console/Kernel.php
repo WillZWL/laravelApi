341946e718e01dfefc855d87d56bebdf0a4f2b9b
gao jinbin <gao1963bin@gmail.com>
2017-01-05 16:59:51 +0800

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -152,8 +152,7 @@ class Kernel extends ConsoleKernel
             ->dailyAt('09:30');
 
          $schedule->command('Iwms:deliveryOrder create',array('--wms' => '4px'))
-            ->dailyAt('01:20');
-            //->everyTenMinutes();
-            
+            ->everyTenMinutes();
+            //->dailyAt('01:20');
     }
 }
