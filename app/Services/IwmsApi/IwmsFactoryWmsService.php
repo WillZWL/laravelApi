05ffc013f07db55ef70f3c431c916b93284a9930
gao jinbin <gao1963bin@gmail.com>
2017-01-11 11:29:21 +0800

    no message

--- app/Services/IwmsApi/IwmsFactoryWmsService.php
+++ app/Services/IwmsApi/IwmsFactoryWmsService.php
@@ -148,7 +148,7 @@ class IwmsFactoryWmsService extends IwmsCoreService
     public function getIwmsCreateDeliveryOrderService()
     {
         if($this->iwmsCreateDeliveryOrderService == null)
-        return $this->iwmsCreateDeliveryOrderService = App::make("App\Services\IwmsApi\Order\IwmsCreateDeliveryOrderService", $this->wmsPlatform);
+        return $this->iwmsCreateDeliveryOrderService = App::make("App\Services\IwmsApi\Order\IwmsCreateDeliveryOrderService", [$this->wmsPlatform]);
     }
 
     public function getIwmsCancelDeliveryOrderService()
