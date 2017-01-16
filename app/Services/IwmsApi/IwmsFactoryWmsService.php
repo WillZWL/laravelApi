e144e33759abc4198a0d6ab036a7df8941f65219
gao jinbin <gao1963bin@gmail.com>
2017-01-16 12:43:00 +0800

    no message

--- app/Services/IwmsApi/IwmsFactoryWmsService.php
+++ app/Services/IwmsApi/IwmsFactoryWmsService.php
@@ -42,12 +42,12 @@ class IwmsFactoryWmsService extends IwmsCoreService
 
     public function cancelDeliveryOrder($esgOrderNoList)
     {
-        $request = $this->getIwmsCancelOrderService()->getDeliveryCancelRequest($esgOrderNoList);
+        $request = $this->getIwmsCancelDeliveryOrderService()->getDeliveryCancelRequest($esgOrderNoList);
         if (!$request["requestBody"]) {
             return false;
         }
         $responseData = $this->curlIwmsApi('wms/cancel-delivery-order', $request["requestBody"]);
-        $this->saveBatchIwmsResponseData($request["batchRequest"],$responseData);
+        return $this->getIwmsCancelDeliveryOrderService()->responseMsgCancelAction($request["batchRequest"], $responseData);
     }
 
     public function sendCreateDeliveryOrderReport()
--- app/Services/IwmsApi/Order/IwmsCancelDeliveryOrderService.php
+++ app/Services/IwmsApi/Order/IwmsCancelDeliveryOrderService.php
@@ -4,7 +4,6 @@ namespace App\Services\IwmsApi\Order;
 
 use App\Models\So;
 use App\Models\IwmsDeliveryOrderLog;
-use Illuminate\Database\Eloquent\Collection;
 
 class IwmsCancelDeliveryOrderService
 {
@@ -16,34 +15,33 @@ class IwmsCancelDeliveryOrderService
     {
         $deliveryCancelRequest = null;
         $batchRequest = $this->getDeliveryCancelRequestBatch($esgOrderSoList);
-        return (!empty($batchRequest)）? $batchRequest : null;
+        return empty($batchRequest) ? null : $batchRequest;
     }
 
     public function getDeliveryCancelRequestBatch($esgOrderSoList)
     {
         $esgAllocateOrder = null; $merchantId = "ESG";
-        $esgOrders = $this->getIwmsDeliveryEsgOrders($esgOrderSoList);
-        if(!$esgOrders->isEmpty()){
-            $batchRequest = $this->getNewBatchId("CANCEL_DELIVERY",$this->wmsPlatform,$merchantId);
-            foreach ($esgOrders as $esgOrder) {
-                foreach ($esgOrder->iwmsDeliveryOrderLog as $iwmsDeliveryOrderLog) {
-                    $deliveryOrderObj = array(
-                        "wms_platform" => $this->wmsPlatform,
-                        "iwms_order_code" => $iwmsWarehouseCode,
-                        "reference_no" => $esgOrder->so_no,
-                    );
-                    $cancelDeliveryOrderObj[] = $deliveryOrderObj;
-                }
+        $iwmsDeliveryOrderLogs = $this->getIwmsDeliveryEsgOrderLogs($esgOrderSoList);
+        if(!$iwmsDeliveryOrderLogs->isEmpty()){
+            $batchRequest = $this->get("CANCEL_DELIVERY",$this->wmsPlatform,$merchantId);
+            foreach ($iwmsDeliveryOrderLogs as $iwmsDeliveryOrderLog) {
+                $deliveryOrderObj = array(
+                    "wms_platform" => $iwmsDeliveryOrderLog->wms_platform,
+                    "merchant_id" => $iwmsDeliveryOrderLog->merchant_id,
+                    "sub_merchant_id" => $iwmsDeliveryOrderLog->sub_merchant_id,
+                    "reference_no" => $iwmsDeliveryOrderLog->reference_no,
+                );
+                $cancelDeliveryOrderObj[] = $deliveryOrderObj;
             }
+            
             $batchRequest->request_log = json_encode($cancelDeliveryOrderObj);
             $batchRequest->save();
             return $batchRequest;
         }
     }
 
-    public function responseMsgCancelAction($postMessage)
+    public function responseMsgCancelAction($batchRequest, $postMessage)
     {
-        IwmsFeedRequest::where("iwms_request_id", $postMessage->request_id)->update(array("status"=> "1","response_log" => json_encode($postMessage->responseMessage)));
         $batchObject = BatchRequest::where("iwms_request_id", $postMessage->request_id)->first();
         if(!empty($batchObject)){
             $batchObject->status = "C";
@@ -51,10 +49,11 @@ class IwmsCancelDeliveryOrderService
             foreach ($postMessage->responseMessage as $key => $responseMessage) {
                 if($key === "success"){
                     foreach ($responseMessage as $value) {
-                        $this->updateEsgDispatchOrderStatusToToShip($value->merchant_order_id, $batchObject->id);
+                        $this->updateEsgDispatchOrderStatusToToShip($value->merchant_order_id);
                     }
                     $subject = "Cancel OMS Delivery Order Success,Please Check Error";
                     $this->sendMsgCancelDeliveryOrderEmail($subject, $responseMessage);
+                    return true;
                 }
                if($key === "failed"){
                     $subject = "Cancel OMS Delivery Order Failed,Please Check Error";
@@ -64,21 +63,14 @@ class IwmsCancelDeliveryOrderService
         }
     }
 
-    public function getIwmsDeliveryEsgOrders($esgOrderSoList)
+    public function getIwmsDeliveryEsgOrderLogs($esgOrderSoList)
     {
-        $deliveryEsgOrders = So::whereIn("so_no",$esgOrderSoList)
-            ->whereHas('iwmsDeliveryOrderLog', function ($query) {
-                $query->whereIn('reference_no', $esgOrderSoList)
+       return IwmsDeliveryOrderLog::whereIn('reference_no', $esgOrderSoList)
                     ->where("status", 1)
-            })
-            ->with("soAllocate")
-            ->with("client")
-            ->with("soItem")
-            ->get();
-        return $deliveryEsgOrders;
+                    ->get();
     }
 
-    private function updateEsgDispatchOrderStatusToToShip($esgSoNo, $batchId)
+    private function updateEsgDispatchOrderStatusToToShip($esgSoNo)
     {
         $esgOrder = So::where("so_no", $esgSoNo)
                         ->with("sellingPlatform")
@@ -87,8 +79,7 @@ class IwmsCancelDeliveryOrderService
         if(empty($esgOrder)){
             return false;
         }
-        IwmsDeliveryOrderLog::where("platform_order_id",$esgOrder->platform_order_id)
-                    ->where("batch_id", $batchId)
+        IwmsDeliveryOrderLog::where("reference_no",$esgOrder->so_no)
                     ->where("status", 1)
                     ->update(array("status" => -1));
         if(!empty($soShipment)){
