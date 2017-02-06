bf4e723a5b6a6555eb9e070063bc8092615bea42
brave <brave.liu@eservicesgroup.com>
2017-02-06 14:28:14 +0800

    no message

--- app/Repository/MarketplaceProductRepository.php
+++ app/Repository/MarketplaceProductRepository.php
@@ -48,42 +48,4 @@ class MarketplaceProductRepository
     {
         return MarketplaceSkuMapping::findOrFail($id);
     }
-
-    public function getMarketplaceProducts($requestData)
-    {
-        $query = MarketplaceSkuMapping::whereMarketplaceId($requestData['marketplace_id'])
-            ->join('product AS p', 'p.sku', '=', 'marketplace_sku_mapping.sku')
-            ->where('marketplace_sku_mapping.country_id', $requestData['country_id'])
-            ->where('marketplace_sku_mapping.status', 1);
-        if (isset($requestData['marketplace_skus']) && $requestData['marketplace_skus']) {
-            $query->whereIn('marketplace_sku_mapping.marketplace_sku', $requestData['marketplace_skus']);
-        }
-        if (isset($requestData['skus']) && $requestData['skus']) {
-            $query->whereIn('p.sku', $requestData['skus']);
-        }
-        if (isset($requestData['colour_id']) && $requestData['colour_id']) {
-            $query->where('p.colour_id', $requestData['colour_id']);
-        }
-        if (isset($requestData['version_id']) && $requestData['version_id']) {
-            $query->where('p.version_id', $requestData['version_id']);
-        }
-        if (isset($requestData['brand_id']) && $requestData['brand_id']) {
-            $query->where('p.brand_id', $requestData['brand_id']);
-        }
-        if (isset($requestData['cat_id']) && $requestData['cat_id']) {
-            $query->where('p.cat_id', $requestData['cat_id']);
-        }
-        if (isset($requestData['sub_cat_id']) && $requestData['sub_cat_id']) {
-            $query->where('p.sub_cat_id', $requestData['sub_cat_id']);
-        }
-        if (isset($requestData['sub_sub_cat_id']) && $requestData['sub_sub_cat_id']) {
-            $query->where('p.sub_sub_cat_id', $requestData['sub_sub_cat_id']);
-        }
-        if (isset($requestData['hscode_cat_id']) && $requestData['hscode_cat_id']) {
-            $query->where('p.hscode_cat_id', $requestData['hscode_cat_id']);
-        }
-        return $query->groupBy('marketplace_sku', 'marketplace_sku_mapping.sku')
-            ->select('marketplace_sku_mapping.*')
-            ->get();
-    }
 }
---
+++ app/Repository/ProductRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\Product;
+
+class ProductRepository
+{
+    public function getProductList($requestData)
+    {
+        $query = Product::where('status', ">=", 1)
+            ->with("marketplaceSkuMapping");
+        if (isset($requestData['skus']) && $requestData['skus']) {
+            $query->whereIn('sku', $requestData['skus']);
+        }
+        if (isset($requestData['colour_id']) && $requestData['colour_id']) {
+            $query->whereColourId($requestData['colour_id']);
+        }
+        if (isset($requestData['version_id']) && $requestData['version_id']) {
+            $query->whereVersionId($requestData['version_id']);
+        }
+        if (isset($requestData['brand_id']) && $requestData['brand_id']) {
+            $query->whereBrandId($requestData['brand_id']);
+        }
+        if (isset($requestData['cat_id']) && $requestData['cat_id']) {
+            $query->whereCatId($requestData['cat_id']);
+        }
+        if (isset($requestData['sub_cat_id']) && $requestData['sub_cat_id']) {
+            $query->whereSubCatId($requestData['sub_cat_id']);
+        }
+        if (isset($requestData['sub_sub_cat_id']) && $requestData['sub_sub_cat_id']) {
+            $query->whereSubSubCatId($requestData['sub_sub_cat_id']);
+        }
+        if (isset($requestData['hscode_cat_id']) && $requestData['hscode_cat_id']) {
+            $query->whereHscodeCatId($requestData['hscode_cat_id']);
+        }
+
+        return $query->groupBy('sku')
+            ->get();
+    }
+}
