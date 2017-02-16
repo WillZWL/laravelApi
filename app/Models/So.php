8d1d4250af5f1992000f5ab8ff68ab45597a3c43
brave <brave.liu@eservicesgroup.com>
2017-02-16 15:59:00 +0800

    no message

--- app/Models/So.php
+++ app/Models/So.php
@@ -119,7 +119,6 @@ class So extends Model
             ->whereMerchantHoldStatus('0')
             ->wherePrepayHoldStatus('0')
             ->whereIsTest('0')
-->limit(2)
             ->get();
     }
 
