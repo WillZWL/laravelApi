ac63704a7a14b62c36e202656c388147a5fb9daf
Milo <milo.chen@eservicesgroup.com>
2017-02-20 16:17:13 +0800

    generate custom invoice

--- app/Services/OrderPackListService.php
+++ app/Services/OrderPackListService.php
@@ -5,12 +5,21 @@ namespace App\Services;
 use App;
 use App\Models\So;
 use App\Models\SoItem;
+use App\Models\SoAllocate;
 use App\Models\Product;
 use App\Models\Currency;
 use App\Models\Country;
+use App\Models\CourierMapping;
 use App\Models\SellingPlatform;
 use App\models\PlatformBizVar;
 use App\Models\ProductAssemblyMapping;
+use App\Models\SupplierProd;
+use App\Models\ExchangeRate;
+use App\Models\ProductCustomClassification;
+use App\models\HscodeDutyCountry;
+use App\models\SkuMapping;
+use App\Models\Declaration;
+use App\Models\DeclarationException;
 use DNS1D;
 use PDF;
 
@@ -34,17 +43,141 @@ class OrderPackListService
         }
     }
 
-    public function generateCustomInvoice($soNo, $courierId)
+    public function generateCustomInvoice($soNo, $courierId = "")
     {
         $soObj = So::where("so_no",$soNo)->first();
         if ($soObj) {
             $result = $this->getCustomInvoice($soObj, $courierId);
+            if ($result) {
+                $returnHTML = view('packlist.custom-invoice',$result)->render();
+                $pickListNo = "PlistNo";
+                $filePath = \Storage::disk('packlist')->getDriver()->getAdapter()->getPathPrefix().$pickListNo;
+                $file = $filePath."/invoice/". $soNo . '.pdf';
+                PDF::loadHTML($returnHTML)->save($file,true);
+                echo $returnHTML;
+            }
         }
     }
 
-    public function getCustomInvoice($soObj)
+    public function getCustomInvoice($soObj, $courierId = "")
     {
+        $result = [];
+        if (!$courierId) {
+            $courierId = $soObj->esg_quotation_courier_id;
+        }
 
+        $isfedex = 0;
+        if (in_array($courierId, ['8', '44', '52', '55', '56', '59'])) {
+            $isfedex = 1;
+        }
+
+        $sellingPlatformObj = $soObj->sellingPlatform;
+        $merchantId = $sellingPlatformObj->merchant_id;
+        $useOptimizedHscodeDuty = $sellingPlatformObj->merchant->use_optimized_hscode_w_duty;
+
+        $shipperName = $courierName = "";
+        $courierMap = CourierMapping::where("courier_id", $courierId)->first();
+        if ($courierMap) {
+            $shipperName = $courierMap->shipper_name;
+        }
+
+        $deliveryCountryObj = Country::where("id", $soObj->delivery_country_id)->first();
+        $declareType = $deliveryCountryObj ? $deliveryCountryObj->declare_type : "FV";
+        $currencyCourierId = $this->getCurrencyCourierId($courierId, $deliveryCountryObj);
+
+        $soExchangeRate = ExchangeRate::where("from_currency_id", $soObj->currency_id)->where("to_currency_id", $currencyCourierId)->first();
+        $soRate = $soExchangeRate->rate;
+
+        $sumItemAmount = 0;
+        $itemDetailDiscount = 0;
+        $useItemDetailDiscount = FALSE;
+        $fedexCustomInvoice = false;
+        $soItem = $soObj->soItem;
+        foreach ($soItem as $item) {
+            if ($item->promo_disc_amt > 0) {
+                $useItemDetailDiscount = true;
+                $itemDetailDiscount += $item->promo_disc_amt;
+            }
+            if ($item->hidden_to_client == 0) {
+                if ($this->isSpecialOrder($soObj)) {
+                    $supplierProd = SupplierProd::where("prod_sku", $item->prod_sku)->where("order_default", 1)->first();
+                    $exchangeRate = ExchangeRate::where("from_currency_id", $supplierProd->currency_id)->where("to_currency_id", $soObj->currency_id)->first();
+                    $item->unit_price = round($exchangeRate->rate * $supplierProd->cost, 2);
+                }
+                $sumItemAmount += $item->unit_price * $item->qty;
+
+                if ((($item->battery == '1') or ($item->battery == '2')) && ($isfedex == 1)) {
+                    $fedexCustomInvoice = true;
+                }
+            }
+        }
+        $sumItemAmount *= $soRate;
+        if ($useItemDetailDiscount) {
+            $discount = $itemDetailDiscount * $soRate;
+        } else {
+            $discount = $soObj->discount * $soRate;
+        }
+        # if must declare full value, force discount to zero
+        if ($declareType == "FV") {
+            $discount = 0;
+        }
+        # prevent negative
+        if($discount > $sumItemAmount) {
+            $discount = $sumItemAmount;
+        }
+
+        $totalOrderAmount = $sumItemAmount - $discount;
+
+        if ($sumItemAmount == 0) {
+            $itemTotalPercent = 0;
+        } else {
+            $itemTotalPercent = 1 - ($discount / $sumItemAmount);
+        }
+
+        $calculateDeclaredValue = $this->pickDeclaredValue($merchantId, $sellingPlatformObj->type, $soObj->delivery_country_id, $totalOrderAmount, $currencyCourierId, $soObj->incoterm);
+
+        $itemResult = [];
+        $declaredValue = 0;
+        foreach ($soItem as $item) {
+            if ($item->hidden_to_client == 0) {
+                $unitPrice = $item->unit_price * $soRate;
+                $unitDeclaredValue = $this->getUnitDeclaredValue($unitPrice, $sumItemAmount, $itemTotalPercent, $totalOrderAmount, $calculateDeclaredValue);
+                //RPX
+                if (in_array($courierId, ['88','91'])) {
+                    $unitDeclaredValue = $item->item_declared_value / $item->qty;
+                }
+                $declaredValue += $unitDeclaredValue * $item->qty;
+                $descAndCode = $this->getDeclaredDescAndCode($item, $useOptimizedHscodeDuty, $soObj->delivery_country_id);
+
+                $isShow = 1;
+                if( (substr($soObj->platform_id,0,2) == "TF" || substr($soObj->platform_id,0,2) == "AC") && $descAndCode["master_sku"] == "34753-MM-BK"){
+                    $isShow = 0;
+                }
+                $itemResult[] = [
+                        "prod_desc" => $descAndCode["prod_desc"],
+                        "code" => $descAndCode["code"],
+                        "qty" => $item->qty,
+                        "unit_declared_value" =>number_format($unitDeclaredValue, 2),
+                        "item_declared_value" =>number_format($unitDeclaredValue * $item->qty, 2),
+                        "is_show" => $isShow
+                    ];
+            }
+        }
+
+        $result["so"] = $soObj;
+        $result["soItem"] = $itemResult;
+        $result["merchant_id"] = $merchantId;
+        $result["courier_id"] = $courierId;
+        $result["shipper"] = $this->getShipperData($soObj->so_no, $shipperName);
+        $result["shipp_to"] = $this->getShipToData($soObj);
+        $result["fedex_custom_invoice"] = $fedexCustomInvoice;
+
+        $result["currency_courier_id"] = $currencyCourierId;
+        $result["total_cost"] = number_format(($declaredValue + $discount),2);
+        $result["total_discount"] = number_format($discount, 2);
+        $result["total_amount"] = number_format(($declaredValue), 2);
+
+        return $result;
     }
 
     public function getDeliveryNote($soObj)
@@ -155,5 +288,219 @@ class OrderPackListService
         }
 
         return $battery;
+    }
+
+    public function getShipperData($soNo, $shipperName = "")
+    {
+        $result = [
+            "shipper_contact" => "",
+            "shipper_name" => $shipperName,
+            "shipper_phone" => "852-35430892",
+            "saddr_1" => "10/F Wah Shing Industrial Building,",
+            "saddr_2" => "18 Cheung Shun Street，",
+            "saddr_3" => "Lai Chi Kok, ",
+            "saddr_4" => "Kowloon,",
+            "saddr_5" => "HongKong",
+            "saddr_6" => "&nbsp;"
+        ];
+        if ($soAllocate = SoAllocate::where("so_no", $soNo)->first()) {
+            if ($soAllocate->warehouse_id == 'ES_DGME') {
+                $result["shipper_name"]    = '';
+                $result["shipper_contact"] = "ME OPS Team";
+                $result["shipper_phone"]   = "+86 (0) 769-26386615";
+                $result["saddr_1"]         = "Cross-Border Ecommerce Park,";
+                $result["saddr_2"]         = "1 Hai Jie Road, ";
+                $result["saddr_3"]         = "Hu Men, ";
+                $result["saddr_4"]         = "Dong Guan,";
+                $result["saddr_5"]         = "Guangdong Province";
+            }
+        }
+        return $result;
+    }
+
+    public function getShipToData($soObj)
+    {
+        $result = [];
+        for ( $i = 1; $i < 7; $i++) {
+            $result["daddr_".$i] = "&nbsp;";
+        }
+
+        $lineNo = 1;
+        list($deliveryAddr1,$deliveryAddr2,$deliveryAddr3) = explode("|",$soObj->delivery_address);
+
+        if($soObj->delivery_company != "")
+        {
+            $result["daddr_".$lineNo] = $soObj->delivery_company;
+            $lineNo++;
+        }
+        $result["daddr_".$lineNo] = $deliveryAddr1;
+        $lineNo++;
+
+        if($deliveryAddr2 != "" || $deliveryAddr3 != "")
+        {
+            if($deliveryAddr2 != "")
+            {
+                $result["daddr_".$lineNo] = $deliveryAddr2;
+                $lineNo++;
+            }
+
+            if($deliveryAddr3 != "")
+            {
+                $result["daddr_".$lineNo] = $deliveryAddr3;
+                $lineNo++;
+            }
+        }
+
+        $csz = "";
+        if($soObj->delivery_city != "")
+        {
+            $csz .= $soObj->delivery_city.", ";
+        }
+        if($soObj->delivery_state != "")
+        {
+            $csz .= $soObj->delivery_state;
+        }
+        if($soObj->delivery_postcode != "")
+        {
+            $csz .= " ".$soObj->delivery_postcode;
+        }
+        $csz = @preg_replace("{, $}","",$csz);
+        if(trim($csz))
+        {
+            $result["daddr_".$lineNo] = $csz;
+            $lineNo++;
+        }
+        $result["daddr_".$lineNo] = $soObj->delivery_country_id;
+
+        return $result;
+    }
+
+    public function isSpecialOrder($soObj)
+    {
+        if ($soObj->biz_type == "SPECIAL" && $soObj->amount == '0.00' && in_array($soObj->sellingPlatform->type, ['TRANSFER', 'ACCELERATOR'])) {
+            return true;
+        }
+        return false;
+    }
+
+    public function pickDeclaredValue($merchantId, $platformType, $deliveryCountryId, $totalOrderAmount, $currencyCourierId, $incoterm)
+    {
+        $byDeclarationException = false;
+        $defaultDeclarationPercent = 1;
+
+        if ($platformType == 'DISPATCH' || $platformType == 'EXPANDER') {
+            $declarationException = DeclarationException::where("platform_type", $platformType)->where("delivery_country_id", $deliveryCountryId)
+                                                    ->where("ref_from_amt", "<=", $totalOrderAmount)->where("ref_to_amt_exclusive", ">", $totalOrderAmount)
+                                                    ->where("status", 1)->where("merchant_id", $merchantId)->first();
+            if (!$declarationException) {
+                $declarationException = DeclarationException::where("platform_type", $platformType)->where("delivery_country_id", $deliveryCountryId)
+                                                    ->where("ref_from_amt", "<=", $totalOrderAmount)->where("ref_to_amt_exclusive", ">", $totalOrderAmount)
+                                                    ->where("status", 1)->where("merchant_id", "ALL")->first();
+            }
+        } else {
+            $declarationException = DeclarationException::where("platform_type", $platformType)->where("delivery_country_id", $deliveryCountryId)
+                                                    ->where("ref_from_amt", "<=", $totalOrderAmount)->where("ref_to_amt_exclusive", ">", $totalOrderAmount)
+                                                    ->where("status", 1)->where("merchant_id", "ALL")->first();
+        }
+
+        if ($declarationException) {
+            $currencyId = $declarationException->currency_id;
+            $declarationRate = ExchangeRate::where("from_currency_id", $currencyId)->where("to_currency_id", $currencyCourierId)->first();
+            $rate = $declarationRate->rate;
+            // declared_value direct by absolute_value
+            if ($declarationException->absolute_value > 0) {
+                $declaredValue = $declarationException->absolute_value * $rate;
+                $byDeclarationException = true;
+            }
+            if ($byDeclarationException === false && $declarationException->declared_ratio > 0) {
+                // declared_value for total_order_amount * declared_percent
+                $declaredValue = $totalOrderAmount * ($declarationException->declared_ratio / 100);
+                // when declared_value > max_absolute_value will be replace for max_absolute_value;
+                $maxAbsoluteValue = $declarationException->max_absolute_value * $rate;
+                if ($maxAbsoluteValue > 0 && $declaredValue > $maxAbsoluteValue) {
+                    $declaredValue = $maxAbsoluteValue;
+                }
+                $byDeclarationException = true;
+            }
+        }
+
+        # no setting declaration_exception,  declared_value = total_order_amount * default_declaration_percent
+        if ($byDeclarationException === false) {
+            $declaration = Declaration::where("platform_type", $platformType)->first();
+            $declaredValue = $totalOrderAmount * ($declaration->default_declaration_percent / 100);
+        }
+
+        #Not rounding, direct two decimal places
+        $declaredValue = floor(($declaredValue) * 100) / 100;
+        $declaredValue = sprintf('%.2f', (float)$declaredValue);
+
+        return $declaredValue;
+    }
+
+    public function getDeclaredDescAndCode($itemObj, $useOptimizedHscodeDuty, $deliveryCountryId)
+    {
+        $code = null;
+        $declaredDesc = "";
+        $product = $itemObj->product;
+        $hscodeCatId = $product->hscode_cat_id;
+
+        if ( in_array($hscodeCatId, ['21','22']) || $hscodeCatId == "" || $useOptimizedHscodeDuty == 0 ) {
+            $productCustomClassification = ProductCustomClassification::where("sku", $product->sku)->where("country_id", $deliveryCountryId)->first();
+            if ($productCustomClassification) {
+                $code = $productCustomClassification->code;
+            }
+            if (strlen($code) > 8) {
+                $code = substr($code, 0, 8);
+            }
+            if (strlen($code) < 8 && strlen($code) > 0) {
+                $code = str_pad($code, 8, '0');
+            }
+            $declaredDesc = $product->declared_desc;
+        } else {
+            $hscodeCategory = $product->hscodeCategory;
+            $hscodeDutyCountry = HscodeDutyCountry::where("hscode_cat_id", $hscodeCatId)->where("country_id", $deliveryCountryId)->first();
+            if ($hscodeDutyCountry) {
+                $code = $hscodeDutyCountry->optimized_hscode;
+            }
+            if (!$code) {
+                $code = $hscodeCategory->general_hscode;
+            }
+            $declaredDesc = $hscodeCategory->name;
+        }
+        $skuMapping = SkuMapping::where("sku", $itemObj->prod_sku)->where("ext_sys", "WMS")->first();
+        $prodDesc = ($skuMapping ? $skuMapping->ext_sku : "") ." ".$declaredDesc;
+
+        return ["code"=>$code, "prod_desc"=>$prodDesc, "master_sku"=>$skuMapping ? $skuMapping->ext_sku : ""];
+    }
+
+    public function getUnitDeclaredValue($unitPrice, $sumItemAmount, $itemTotalPercent, $totalOrderAmount, $calculateDeclaredValue)
+    {
+        if ($sumItemAmount == 0) {
+            $unitDeclaredPercent = 0;
+        } else {
+            $unitPriceValue = $unitPrice * $itemTotalPercent;
+            $unitDeclaredPercent = $unitPriceValue / ($totalOrderAmount ? $totalOrderAmount : $sumItemAmount);
+        }
+        return $unitDeclaredValue = $calculateDeclaredValue * $unitDeclaredPercent;
+    }
+
+    public function getCurrencyCourierId($courierId, $deliveryCountryObj)
+    {
+        switch ($courierId) {
+            // PostNL
+            case 69:
+            case 70:
+            case 133:
+                $currencyCourierId = "USD";
+                break;
+            case 88:
+            case 91:
+                $currencyCourierId = "EUR";
+                break;
+            default:
+                $currencyCourierId = $deliveryCountryObj->currency_courier_id;
+                break;
+        }
+        return $currencyCourierId;
     }
 }
--- resources/views/packlist/custom-invoice.blade.php
+++ resources/views/packlist/custom-invoice.blade.php
@@ -4,7 +4,6 @@
 <title>Custom Invoice</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <style type="text/css">
-   .pb{page-break-after : always;}
    body {margin:0; text-align:center;}
    * {font-family:Helvetica,rial,verdana,sans-serif;font-size:11px;}
   .field{background-color:#e1e1e1; padding-left:5px; text-align:left; height:25px; font-size:12px;}
@@ -14,8 +13,173 @@
 </head>
 <body marginwidth='0' marginheight='0'>
 <div style='width:100%;'>
+    <table width='100%' cellpadding='0' cellspacing='0' border='0'>
+        <tr><td colspan='3' align='center'><b style='font-size:16px;'>Custom Invoice</b><br/><br/></td></tr>
+        <tr>
+            <td width='10%'>&nbsp;</td>
+            <td width='80%'>
+            <table width='100%' cellpadding='0' cellspacing='1' border='0' bgcolor='#000000'>
+                <tr>
+                    <td class='field' width='20%'>Date of Invoice</td>
+                    <td class='value' width='30%'>{{ date("d/m/Y") }}</td>
+                    <td class='field' width='20%'>Order No.</td>
+                    <td class='value' width='30%'>{{ $so->client_id }} - {{ $so->so_no }}</td>
+                </tr>
+                <tr>
+                    <td class='field'>Shipper Name</td>
+                    <td class='value'>{{ $shipper['shipper_name'] }}</td>
+                    <td class='field'>Ship to</td>
+                    <td class='value'>{{ $so->delivery_name }}</td>
+                </tr>
+                <tr>
+                    <td class='field' rowspan='6' valign='top' style='padding-top:4px;'>Shipper Address</td>
+                    <td class='value'>{{ $shipper['saddr_1'] }}</td>
+                    <td class='field' rowspan='6' valign='top' style='padding-top:4px;'>Ship to Address</td>
+                    <td class='value'>{{ $shipp_to['daddr_1'] }}</td>
+                </tr>
+                <tr>
+                    <td class='value'>{{ $shipper['saddr_2'] }}</td>
+                    <td class='value'>{{ $shipp_to['daddr_2'] }}</td>
+                </tr>
+                <tr>
+                    <td class='value'>{{ $shipper['saddr_3'] }}</td>
+                    <td class='value'>{{ $shipp_to['daddr_3'] }}</td>
+                </tr>
+                <tr>
+                    <td class='value'>{{ $shipper['saddr_4'] }}</td>
+                    <td class='value'>{{ $shipp_to['daddr_4'] }}</td>
+                </tr>
+                <tr>
+                    <td class='value'>{{ $shipper['saddr_5'] }}</td>
+                    <td class='value'>{{ $shipp_to['daddr_5'] }}</td>
+                </tr>
+                <tr>
+                    <td class='value'>{{ $shipper['saddr_6'] }}</td>
+                    <td class='value'>{{ $shipp_to['daddr_6'] }}</td>
+                </tr>
+                <tr>
+                    <td class='field'>Contact person</td>
+                    <td class='value'>{{ $shipper['shipper_contact'] }}</td>
+                    <td class='field'>Contact person</td>
+                    <td class='value'>{{ $so->delivery_name }}</td>
+                </tr>
+                <tr>
+                    <td class='field'>Contact no.</td>
+                    <td class='value'>{{ $shipper['shipper_phone'] }}</td>
+                    <td class='field'>Contact no.</td>
+                    <td class='value'>{{ $so->del_tel_1 }}-{{ $so->del_tel_2 }}-{{ $so->del_tel_3 }}</td>
+                </tr>
+            </table>
+            <br/>
+            <div style='float:left;'>
+                <b style='font-size:16px;'>Incoterm:
+                @if($so->incoterm == "DDP" && in_array($courier_id,["45","51"]))
+                DDP for all VAT duties, taxes and handling fees
+                @else
+                {{ $so->incoterm }}
+                @endif
+                </b>
+            </div>
+            <br/>
+            <br/>
+        <table border='0' cellpadding='0' cellspacing='0' width='100%'>
+        <tr bgcolor='#000000'>
+            <td colspan='13' height='1'></td>
+        </tr>
+        <tr>
+            <td width='1' class='tborder'></td>
+            <td class='field'>Product descriptions</td>
+            <td width='1' class='tborder'></td>
+            <td class='field'>Country Of Origin</td>
+            <td width='1' class='tborder'></td>
+            <td class='field'>HS Code</td>
+            <td width='1' class='tborder'></td>
+            <td class='field'>Qty</td>
+            <td width='1' class='tborder'></td>
+            <td width='15%' class='field'>Unit Value({{ $currency_courier_id }})</td>
+            <td width='1' class='tborder'></td>
+            <td width='15%' class='field'>Total Value({{ $currency_courier_id }})</td>
+            <td width='1' class='tborder'></td>
+        </tr>
+        <tr bgcolor='#000000'>
+            <td colspan='13' height='1'></td>
+        </tr>
+        @foreach($soItem as $item)
+            @if($item["is_show"])
+            <tr bgcolor='#000000'>
+                <td width='1' class='tborder'></td>
+                <td class='value'>{{ $item['prod_desc'] }}</td>
+                <td width='1' class='tborder'></td>
+                <td class='value'>China</td>
+                <td width='1' class='tborder'></td>
+                <td class='value'>{{ $item['code'] }}</td>
+                <td width='1' class='tborder'></td>
+                <td class='value'>{{ $item['qty'] }}</td>
+                <td width='1' class='tborder'></td>
+                <td class='value'>{{ $item['unit_declared_value'] }}</td>
+                <td width='1' class='tborder'></td>
+                <td class='value'>{{ $item['item_declared_value'] }}</td>
+                <td width='1' class='tborder'></td>
+            </tr>
+            <tr bgcolor='#000000'><td colspan='13' height='1'></td></tr>
+            @endif
+        @endforeach
+        <tr>
+            <td colspan='8' rowspan='10' class='value'></td>
+            <td width='1' class='tborder'></td>
+            <td class='field'>Original Cost of Items</td>
+            <td width='1' class='tborder'></td>
+            <td class='value'>{{ $total_cost }}</td>
+            <td width='1' class='tborder'></td>
+        </tr>
+        <tr bgcolor='#000000'>
+            <td colspan='10' height='1'></td>
+        </tr>
+        <tr>
+            <td width='1' class='tborder'></td>
+            <td class='field'>Total Discount Applied</td>
+            <td width='1' class='tborder'></td>
+            <td class='value'>{{ $total_discount }}</td>
+            <td width='1' class='tborder'></td>
+        </tr>
+        <tr bgcolor='#000000'>
+            <td colspan='10' height='1'></td>
+        </tr>
+        <tr>
+            <td width='1' class='tborder'></td>
+            <td class='field'>Total Amount Charged</td>
+            <td width='1' class='tborder'></td>
+            <td class='value'>{{ $total_amount }}</td>
+            <td width='1' class='tborder'></td>
+        </tr>
+        <tr bgcolor='#000000'>
+            <td colspan='5' height='1'></td>
+        </tr>
 
-
-      <p class="pb"></p>
+        </table>
+        <br/><br/>
+        <div style='width:100%; text-align:left; font-size:12px; position:relative'>
+            @if($merchant_id == "LUMOS" && $so->delivery_country_id == "GB")
+            Package is a bicycle helmet. As a piece of personal protective equipment, this product is zero-rated under UK law and therefore is not subject to VAT<br/><br/>
+            @endif
+            I declare that the above information is true and correct and to the best of our knowledge. The product(s) covered by this document are not subject to any export or import prohibitions & restrictions.<br/><br/>
+            <div style="float: right;bottom:5px;right:0; border:1px solid black;width:220px;height: 70px;text-align: center;vertical-align: middle;">
+                <img src="data:image/png;base64,{{ DNS1D::getBarcodePNG($so->so_no, 'C128') }}" alt="barcode" style="padding-top: 10px;" /><br/>
+                <span style="font-size: 16px;font-weight: 600;padding-left: 32px;">{{ $so->so_no }}</span></p>
+            </div>
+        </div>
+        @if($fedex_custom_invoice)
+        <div style='width:100%;text-align:left;clear:both;font-size:12px;'>
+                         <b>Package contains lithium ion batteries or cells (PI966) </b><br><br>
+                        Handle with care, flammability hazard if damage <br>
+                        Special procedures must be followed in the event the package is damaged, <br>
+                        to include inspection and repacking if necessary <br>
+                        Emergency contact no. +852 3153 2766<br></div>
+        @endif
+        </td>
+        <td width='10%'>&nbsp;</td>
+    </tr>
+    </table>
+</div>
 </body>
 </html>
