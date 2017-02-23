b2c69eeedaf3c590796bd139fcf510f229a2f81d
Will Zhang <will.zhang@eservicesgroup.com>
2017-02-23 10:59:31 +0800

    use .env to store iwms api && access_token

--- config/iwms.php
+++ config/iwms.php
@@ -13,6 +13,6 @@ return [
      */
     //"url" => "http://iwms.dev/api/wms/",
     "url" => env('IWMS_API_URL', "http://iwms.eservicesgroup.com/api/"),
-    "accessToken" => env('', "bf7238837236f822c88126e571a730a2ad733dddf718f8c148786096915516864515191dcfdd0ce3"),
+    "accessToken" => env('IWMS_ACCESS_TOKEN', "bf7238837236f822c88126e571a730a2ad733dddf718f8c148786096915516864515191dcfdd0ce3"),
 ];
 
