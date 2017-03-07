7837458615004a24e087b4b4abbd629edc498b4e
brave <brave.liu@eservicesgroup.com>
2017-03-07 14:16:09 +0800

    no message

--- app/Services/ShipperNotAvailableOrderService.php
+++ app/Services/ShipperNotAvailableOrderService.php
@@ -19,77 +19,22 @@ class ShipperNotAvailableOrderService
 
     public function getData()
     {
-        $cellItems = [];
         $orders = $this->getShipperNotAvailableOrders();
         if (! $orders->isEmpty()) {
             foreach ($orders as $order) {
-                if ($order->status != 6) {
+                if ($order->status > 0 && $order->status < 6) {
                     \DB::connection('mysql_esg')->beginTransaction();
                     try {
                         $reason = "Shipper Not Available";
                         $this->saveSoHoldReason($order->so_no, $reason);
                         $order->hold_status = 4;
                         $order->save();
-                        switch ($order->courier_status) {
-                            case '1':
-                                $courierStatus = "Active";
-                                break;
-
-                            case '0':
-                                $courierStatus = "Inactive";
-                                break;
-
-                            default:
-                                $courierStatus = "-";
-                                break;
-                        }
-                        $courierStatus =  $courierStatus;
-                        $cellItems[$order->so_no] = [
-                            $order->so_no,
-                            $order->order_create_date,
-                            $order->status,
-                            $reason,
-                            $order->courier_id ? $order->courier_id : '-',
-                            $order->courier_name ? $order->courier_name : '-',
-                            $courierStatus,
-                        ];
                         \DB::connection('mysql_esg')->commit();
                     } catch (\Exception $e) {
-                        if (isset($cellItems[$order->so_no])) {
-                            unset($cellItems[$order->so_no]);
-                        }
                         \DB::connection('mysql_esg')->rollBack();
                         echo $e->getMessage(). ", Line: ".$e->getLine();
                     }
                 }
-            }
-        }
-        if ($cellItems) {
-            $headerData[] = [
-                'So NO.',
-                'Order Create Date',
-                'Order Status',
-                'hold Reason',
-                'Courier ID',
-                'Courier Name',
-                'Courier Status',
-            ];
-            $cellDatas = array_merge($headerData, $cellItems);
-            $path = storage_path().'/app/ShipperNotAvailableOrder';
-            $fileName = "HoldOrderForShipperNotAvailable";
-
-            $excelFile = $this->createExcelFile($fileName, $path, $cellDatas);
-            if($excelFile){
-                $subject = "[ESG] Alert, hold order for shipper not available";
-                $this->sendAttachmentMail(
-                    'mec.team@eservicesgroup.com, ',
-                    $subject,
-                    [
-                        'path' => $path,
-                        'file_name' => $fileName.'.xlsx'
-                    ],
-                    'it@eservicesgroup.net'
-                );
             }
         }
     }
