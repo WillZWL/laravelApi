bd2e379ca03194e21768aa67fbe04905b2730cd7
Will Zhang <will.zhang@eservicesgroup.com>
2017-03-15 17:45:07 +0800

    add select

--- app/Services/OrderSettlementService.php
+++ app/Services/OrderSettlementService.php
@@ -18,7 +18,7 @@ class OrderSettlementService
         if ($type = $request->get('type')) {
             $query->where('sp.type', '=', $type);
         }
-        // $query->select('sps.payment_gateway_id, so.biz_type AS biz_type, so.txn_id AS txn_id, so.so_no, so.platform_id, so.order_create_date, so.dispatch_date, so.currency_id, so.amount, so.settlement_date, m.marketplace_contact_name, m.marketplace_email_1, m.marketplace_email_2, m.marketplace_email_3, m.marketplace_contact_phone');
+        $query->select('sps.payment_gateway_id', 'so.biz_type', 'so.txn_id', 'so.so_no', 'so.platform_order_id', 'so.order_create_date', 'so.dispatch_date', 'so.currency_id', 'so.amount', 'so.settlement_date', 'so.create_on', 'm.marketplace_contact_name', 'm.marketplace_contact_phone', 'm.marketplace_email_1', 'm.marketplace_email_2', 'm.marketplace_email_3');
         $per_page = 10;
         if ($request->get('per_page')) {
             $per_page = $request->get('per_page');
