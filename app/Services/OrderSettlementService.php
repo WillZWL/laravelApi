1a98a187769db26aa6b3b9b0bbc54dd56ea7187e
Will Zhang <will.zhang@eservicesgroup.com>
2017-03-16 13:32:21 +0800

    Order Settlement List

--- app/Services/OrderSettlementService.php
+++ app/Services/OrderSettlementService.php
@@ -18,6 +18,18 @@ class OrderSettlementService
         if ($type = $request->get('type')) {
             $query->where('sp.type', '=', $type);
         }
+        if ($payment_gateway = $request->get('payment_gateway')) {
+            $query->where('sps.payment_gateway_id', $payment_gateway);
+        }
+        if ($biz_type = $request->get('biz_type')) {
+            $query->where('so.biz_type', $biz_type);
+        }
+        if ($txn_id = $request->get('txn_id')) {
+            $query->where('so.txn_id', $txn_id);
+        }
+        if ($so_no = $request->get('so_no')) {
+            $query->where('so.so_no', $so_no);
+        }
         $query->select('sps.payment_gateway_id', 'so.biz_type', 'so.txn_id', 'so.so_no', 'so.platform_order_id', 'so.order_create_date', 'so.dispatch_date', 'so.currency_id', 'so.amount', 'so.settlement_date', 'so.create_on', 'm.marketplace_contact_name', 'm.marketplace_contact_phone', 'm.marketplace_email_1', 'm.marketplace_email_2', 'm.marketplace_email_3');
         $per_page = 10;
         if ($request->get('per_page')) {
